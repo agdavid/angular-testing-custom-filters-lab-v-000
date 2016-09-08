c1fec4dee49c6d00128ee3d19f55ea770b3aeaf7
agdavid <antonio.g.david@gmail.com>
2016-09-08 10:34:14 -0400

    write unit test for filter favoriteFood

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,18 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favoriteFood;
 
 	beforeEach(module('app'));
 
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
+
+    //set var to actual filter
+    favoriteFood = $filter('favoriteFood');
 	}));
 
+  it('should return the correct array of objects where object.favoriteFood is the listed food', function() {
+    expect(favoriteFood([{animal: 'pig', favoriteFood: 'pizza'},{animal: 'cow', favoriteFood: 'taco'}],'pizza')).toEqual([{animal: 'pig', favoriteFood: 'pizza'}]);
+    expect(favoriteFood([{animal: 'pig', favoriteFood: 'pizza'},{animal: 'cow', favoriteFood: 'taco'}],'pizza').length).toEqual(1);
+  });
 	
 });
