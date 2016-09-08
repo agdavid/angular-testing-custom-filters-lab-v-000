ecfb9447adbfa5a5a5809d6c0b789b578929e73a
agdavid <antonio.g.david@gmail.com>
2016-09-08 10:39:34 -0400

    write unit test for filter removeAllVowels

--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,18 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $controller, removeAllVowels;
 
 	beforeEach(module('app'));
 
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
+
+    //set var to actual fitler
+    removeAllVowels = $filter('removeAllVowels');
 	}));
+
+  it('should remove all the vowels from a string', function() {
+    expect(removeAllVowels('umbrella')).toEqual('mbrll');
+    expect(removeAllVowels('aeiou').length).toEqual(0);
+  });
 
 });
